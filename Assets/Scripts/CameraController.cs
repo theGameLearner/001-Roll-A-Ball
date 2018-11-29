c13605028e7b3bb325ce28a9d5c8224e9083e54a
TheGameLearner <it.rishabh.jain@gmail.com>
2018-11-29 10:44:52 +0530

    The complete Project
    The project was created by following tutorials and was the starting point of me entering Unity

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+
+	public GameObject Player1;
+	private Vector3 offset;
+	//offset = position(camera) - position(player)
+
+	// Use this for initialization
+	void Start () 
+	{
+		offset = transform.position - Player1.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		//LateUpdate() runs after all objects are done with Update in a frame
+		// This ensures that camera takes image after all objects are set
+		transform.position = Player1.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+	private int count;
+	public Text countText;
+	public Text winText;
+
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate () 
+	{
+		//Called Before performing physics Calculations
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		// float xSpeed = xMove * speed; // float zSpeed = zMove * speed;
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		//Destroy (other.gameObject);
+		//other.gameObject.CompareTag("Player")
+		//gameObject.SetActive(false);
+		if (other.gameObject.CompareTag ("Pick Up")) 
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count : "+count.ToString();
+		if (count == 13) 
+		{
+			winText.text = "You Win";
+		}
+	}
+}﻿
